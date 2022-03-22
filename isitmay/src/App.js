0cd3024cfc237d40b359f989e45f8ca21a84d89b
Connor Sullivan <crsullivan@protonmail.com>
2022-03-22 10:41:48 -0700

    React Framework, app done

---
+++ isitmay/src/App.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+function App() {
+
+  
+  const [monthStatus, setmonthStatus] = useState()
+
+  async function getmonthStatus() {
+    if (month.getMonth() == 4) {
+      setmonthStatus('Yes')
+    }
+    else {
+      setmonthStatus('No')
+    }
+  }
+  useEffect(() => {
+    getmonthStatus();
+    }, []);
+
+  
+  const month = new Date()
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      {monthStatus}
+      </header>
+    </div>
+  );
+}
+
+export default App;
