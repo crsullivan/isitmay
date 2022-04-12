fbb92512392df91efd76dc6c12ac8c38c91fa671
Connor Sullivan <crsullivan@protonmail.com>
2022-04-12 14:21:18 -0700

    Adds randomizer and options

--- isitmay/src/App.js
+++ isitmay/src/App.js
@@ -8,10 +8,24 @@ function App() {
 
   async function getmonthStatus() {
     if (month.getMonth() === 4) {
-      setmonthStatus('Yes')
+      setmonthStatus('Yes!')
     }
     else {
-      setmonthStatus('Survey says: No')
+      var options = [
+        'Nope',
+        'Survey says: No!',
+        'Probably not',
+        'Yes!!! Wait... Actually no, sorry.',
+        'Better luck next time!',
+        'Definitely not',
+        'A watched pot never boils',
+        'It is not May',
+        "Not for you it isn't...",
+        "What even is 'a month' anyway? Seems arbitrary"
+        ]
+        let myoptions = options[Math.floor(Math.random() * options.length)]
+        console.log(myoptions)//This gives you any string from groceries 
+      setmonthStatus(myoptions)
     }
   }
   useEffect(() => {
